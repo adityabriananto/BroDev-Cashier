4453bb79e4f349ddec3ee5053d472301c92f2de9
Briananto, Aditya <aditya.briananto@lazada.co.id>
2026-06-22 16:02:06 +0700

    fix spacing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-{
-    $this->call(ProductSeeder::class);
-}
+    {
+        $this->call(ProductSeeder::class);
+    }
 }
